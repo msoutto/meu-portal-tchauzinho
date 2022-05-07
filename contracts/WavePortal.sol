56f82f16a2a7075bf3f076c1f3f8636ece9b3fac
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-07 18:51:08 -0300

    1- Adding wave feature and customizing a lil

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,23 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping (address => uint256) addressWaves;
+
     constructor() {
-        console.log("Salve, eu sou um contrato e eu sou inteligente");
+        console.log("Fala ai!! Eu sou um contrato e eu sou inteligente! ;)");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        addressWaves[msg.sender] += 1;
+
+        console.log("%s mandou um salve!", msg.sender);
+        console.log("%s ja mandou %d salve(s) no total!", msg.sender, addressWaves[msg.sender]);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("Temos um total de %d salves!", totalWaves);
+        return totalWaves;
     }
 }
