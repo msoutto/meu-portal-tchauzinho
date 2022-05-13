3a5353c2581b8a9a71d221b006451f95a0434199
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-13 22:39:52 +0000

    Session3_Lesson2: Adding money to contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,51 +8,43 @@ contract WavePortal {
   
   struct WaveData {
     address waver;
-    uint256 timestamp; // Data/hora de quando o usuário salve.
-    string message; // Mensagem que o usuário enviou
+    uint256 timestamp;
+    string message;
   }
 
   mapping (address => uint256[]) waverToWaves;
   WaveData[] waves;
   uint256 totalWaves;
 
-  /*
-  * Um pouco de mágica, use o Google para entender o que são eventos em Solidity!
-  */
   event NewWave(address indexed from, uint256 timestamp, string message);
   
-  constructor() {
-    console.log("Fala ai!! Eu sou um contrato e eu sou inteligente! ;)");
+  constructor() payable {
+    console.log("Contrato publicado!");
   }
 
   function wave(string memory _message) public {
     console.log("%s mandou um salve com a mensagem: %s", msg.sender, _message);
     totalWaves += 1;
     
-    /*
-    * Popular os waves de um waver
-    */
     waves.push(WaveData(msg.sender, block.timestamp, _message));
 
-    /*
-    * Estou usando o mapping para saber se o endereço é novo (lista de indices de waves vazia). Assim, não tenho que iterar sobre todos os endereços para descobrir.
-    * Populando a lista de indices de waves do mapping com o último indice da lista de waves (que acabou de ser inserido).
-    */
     if (waverToWaves[msg.sender].length == 0) {
       waverToWaves[msg.sender].push(waves.length - 1);
     }
 
     console.log("%s ja mandou %d salve(s) no total!", msg.sender, waverToWaves[msg.sender].length);
-    /*
-    * Eu adicionei algo novo aqui. Use o Google para tentar entender o que é e depois me conte o que aprendeu em #general-chill-chat
-    */
+    
     emit NewWave(msg.sender, block.timestamp, _message);
+
+    uint256 prizeAmount = 0.00001 ether;
+    require(
+        prizeAmount <= address(this).balance,
+        "Tentando sacar mais dinheiro que o contrato possui."
+    );
+    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+    require(success, "Falhou em sacar dinheiro do contrato.");
   }
 
-  /*
-  * Adicionei uma função getAllWaves que retornará os salves.
-  * Isso permitirá recuperar os salves a partir do nosso site!
-  */
   function getAllWaves() public view returns (WaveData[] memory) {
     return waves;
   }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,34 @@
 const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.01")
+  });
   await waveContract.deployed();
   console.log("Endereço do contrato:", waveContract.address);
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
+  /*
+   * Consulta saldo do contrato
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Saldo do contrato:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let waveTxn = await waveContract.wave("Uma mensagem de teste!");
   await waveTxn.wait(); // aguarda a transação ser minerada
-  
-  const [_, randomPerson] = await hre.ethers.getSigners();
-  waveTxn = await waveContract.connect(randomPerson).wave("Outra mensagem!");
-  await waveTxn.wait(); // aguarda a transação ser minerada
 
+  /*
+   * Recupera o saldo do contrato para verificar o que aconteceu!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Saldo do  contrato:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+  
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
 };
