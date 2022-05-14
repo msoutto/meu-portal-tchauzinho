a4aa968907cf054a72303e4c6e68d63ee6f53aee
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-14 14:16:25 +0000

    Session4_Lesson2- using env vars to get keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
+// por algum motivo, não estou conseguindo utilizar as variáveis de ambiente do repl assim... e não posso usar o dotenv no repl...
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: process.env.STAGING_ALCHEMY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+      url: process.env['STAGING_ALCHEMY_KEY'],
+      accounts: [process.env['PRIVATE_KEY']],
     },
     mainnet: {
       chainId: 1,
       url: process.env.PROD_ALCHEMY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env['PRIVATE_KEY']],
     },
   },
 };
