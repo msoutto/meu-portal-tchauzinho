d1c386a3ef4ff0c8cb690631c7934d9dc86ea6ce
msoutto <dev.msoutto@gmail.com>
2025-07-04 10:48:15 -0300

    Session1_Lesson1 restarting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
-// parece que ao importar o dotenv, o repl se confunde...
-// ao rodar local, importar o config do dotenv:
-//require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.0",
-  networks: {
-    rinkeby: {
-      url: process.env['STAGING_ALCHEMY_KEY'],
-      accounts: [process.env['PRIVATE_KEY']],
-    },
-    /*
-    * apenas habilitar caso deseja conectar à mainnet
-    mainnet: {
-      chainId: 1,
-      url: process.env['PROD_ALCHEMY_KEY'], 
-      accounts: [process.env['PRIVATE_KEY']],
-    },
-    */
-  },
+  solidity: "0.8.28",
 };
